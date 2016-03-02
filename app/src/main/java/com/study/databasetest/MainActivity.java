fc44e5976465e666217d09951b210092a067e38f
WXB506 <1922686887@qq.com>
2016-03-02 23:18:02 +0800

    query data from database

--- app/src/main/java/com/study/databasetest/MainActivity.java
+++ app/src/main/java/com/study/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.study.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -55,16 +57,39 @@ public class MainActivity extends AppCompatActivity {
                 ContentValues values = new ContentValues();
 
                 values.put("price", 10.99);
-                db.update("Book", values, "name = ?", new String[] {"The Da Vinci Code" });
+                db.update("Book", values, "name = ?", new String[]{"The Da Vinci Code"});
             }
         });
 
-        final Button deleteData = (Button) findViewById(R.id.delete_data);
+        Button deleteData = (Button) findViewById(R.id.delete_data);
         deleteData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 db.delete("Book", "pages > ?", new String[] {"500"});
+            }
+        });
+
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if(cursor.moveToFirst()) {
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d("MainAcitity", "book name is " + name);
+                        Log.d("MainAcitity", "book author is " + author);
+                        Log.d("MainAcitity", "book pages is " + pages);
+                        Log.d("MainAcitity", "book price is " + price);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
